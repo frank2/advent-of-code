25e803c652c35cd98dad995b6557ef4c954efb61
frank2 <frank2@dc949.org>
2021-12-07 16:02:48 -0800

    add problem 6, RUDE PUZZLE

---
+++ 06_Lanternfish/src/main.rs
@@ -0,0 +1,55 @@
+use std::io;
+use std::collections::HashMap;
+
+fn simulate(fish: &Vec<u8>, steps: u16) -> u64 {
+    let mut breedable = [0u64; 7];
+    let mut available = [0u64; 7];
+
+    for f in fish {
+        breedable[*f as usize] += 1;
+        available[*f as usize] += 1;
+    }
+    for s in 0..steps {
+        let modulo = (s % 7) as usize;
+        let delay = ((s + 2) % 7) as usize;
+
+        available[delay] = available[delay] + breedable[modulo];
+        breedable[modulo] = available[modulo];
+    }
+
+    available.iter().sum()
+}
+
+fn read_fish() -> Result<Vec<u8>, ()> {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+
+    if let Ok(size) = stdin.read_line(&mut buffer) {
+        let mut fish: Vec<u8> = buffer.trim()
+            .split(",")
+            .map(|x| x.parse().unwrap())
+            .collect();
+
+        Ok(fish)
+    }
+    else { Err(()) }
+}
+
+fn part1() {
+    if let Ok(mut fish) = read_fish() {
+        println!("{}", simulate(&fish, 80));
+    }
+    else { panic!("couldn't read fish!"); }
+}
+
+fn part2() {
+    if let Ok(mut fish) = read_fish() {
+        println!("{}", simulate(&fish, 256));
+    }
+    else { panic!("couldn't read fish!"); }
+}
+
+fn main() {
+    // part1();
+    part2();
+}
